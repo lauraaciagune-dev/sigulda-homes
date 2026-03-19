7749200f501ba4c2e9efedce907144f20dff83bb
lauraaciagune-dev <lauraa.ciagune@gmail.com>
2026-03-19 19:50:29 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,9 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 export const metadata = {
   title: "Siguldas mājas modern page",
   description:
